e1f28d2a576fa154957c539048f3e3db399daf64
Victor Balanica <victorbalanica@gmail.com>
2014-12-13 23:20:47 +0000

    UD032 : L2P2 input data file + successful solution for 'Airport List' problem

---
+++ UD032/L2P2/airports.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# All your changes should be in the 'extract_airports' function
+# It should return a list of airport codes, excluding any combinations like "All"
+
+__author__ = 'zaorish'
+
+from bs4 import BeautifulSoup
+html_page = "options.html"
+
+
+def extract_airports(page):
+    data = []
+    with open(page, "r") as html:
+        soup = BeautifulSoup(html)
+        carrier_list = soup.find(id='AirportList')
+        for option in carrier_list.find_all('option'):
+            if not option['value'].startswith('All'):
+                data.append(option['value'])
+    return data
+
+
+def test():
+    data = extract_airports(html_page)
+    assert len(data) == 15
+    assert "ATL" in data
+    assert "ABR" in data
+
+test()
